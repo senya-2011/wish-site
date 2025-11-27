f0cbd26b561354ffdbe23e2ba222007e57733dd3
Arseniy Alhimovich <144542914+senya-2011@users.noreply.github.com>
2025-11-27 18:53:31 +0300

    feat(openapi): add openapi.yaml

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	kotlin("plugin.spring") version "2.2.21"
 	id("org.springframework.boot") version "4.0.0"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("org.openapi.generator") version "7.6.0"
 }
 
 group = "com.dabwish"
@@ -18,6 +19,8 @@ java {
 repositories {
 	mavenCentral()
 }
+
+val openApiOutputDir = layout.buildDirectory.dir("generated/openapi")
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
@@ -35,7 +38,11 @@ dependencies {
     implementation("org.mapstruct:mapstruct:1.6.3")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
 
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
+    //openapi
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("io.swagger.core.v3:swagger-annotations:2.2.20")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -47,6 +54,33 @@ kotlin {
 	}
 }
 
+openApiGenerate {
+	generatorName.set("kotlin-spring")
+	inputSpec.set("$rootDir/openapi/openapi.yml")
+	outputDir.set(openApiOutputDir.get().asFile.absolutePath)
+	apiPackage.set("com.dabwish.dabwish.generated.api")
+	modelPackage.set("com.dabwish.dabwish.generated.dto")
+	configOptions.set(
+		mapOf(
+			"interfaceOnly" to "true",
+			"useBeanValidation" to "true",
+			"useSpringBoot3" to "true",
+			"dateLibrary" to "java8"
+		)
+	)
+}
+
+sourceSets {
+	named("main") {
+		kotlin.srcDir(openApiOutputDir.map { it.dir("src/main/kotlin").asFile }.get())
+		resources.srcDir(openApiOutputDir.map { it.dir("src/main/resources").asFile }.get())
+	}
+}
+
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.named("compileKotlin") {
+	dependsOn("openApiGenerate")
 }
--- src/main/kotlin/com/dabwish/dabwish/mapper/UserMapper.kt
+++ src/main/kotlin/com/dabwish/dabwish/mapper/UserMapper.kt
@@ -1,11 +1,9 @@
 package com.dabwish.dabwish.mapper
 
-import com.dabwish.dabwish.dto.user.ViewUser
 import com.dabwish.dabwish.model.user.User
 import org.mapstruct.Mapper
 
 @Mapper(componentModel = "spring")
 interface UserMapper {
-    fun userToViewUser(user: User): ViewUser
-    fun viewUserToUser(userViewUser: ViewUser): User
+
 }
