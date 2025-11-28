6ef480bc5e5251b519f31188c3e43c4b68aa31b3
Arseniy Alhimovich <144542914+senya-2011@users.noreply.github.com>
2025-11-29 02:51:47 +0300

    notification-service: feat(WishEventListener): add update/create listener

---
+++ notification-service/src/main/kotlin/com/wish_notification/notification_service/kafka/WishEventListener.kt
@@ -0,0 +1,43 @@
+package com.wish_notification.notification_service.kafka
+
+import com.dabwish.events.wish.WishCreatedEvent
+import com.dabwish.events.wish.WishUpdatedEvent
+import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.stereotype.Component
+
+@Component
+@ConditionalOnProperty(value = ["app.kafka.enabled"], havingValue = "true", matchIfMissing = true)
+class WishEventListener {
+    private val log = LoggerFactory.getLogger(this::class.java)
+
+    @KafkaListener(
+        topics = ["\${app.kafka.topics.wish-created:wish-created-events}"],
+    )
+    fun handleWishCreated(event: WishCreatedEvent) {
+        log.info(
+            "New Wish Created! [ID: {}] Owner: {}, Title: '{}', Price: {}, CreatedAt: {}",
+            event.wishId,
+            event.ownerId,
+            event.title,
+            event.price ?: "N/A",
+            event.createdAt
+        )
+
+    }
+
+    @KafkaListener(
+        topics = ["\${app.kafka.topics.wish-updated:wish-updated-events}"],
+    )
+    fun handleWishUpdated(event: WishUpdatedEvent) {
+        log.info(
+            "Wish Updated [ID: {}] Owner: {}. Changed: Title='{}', Price={}, UpdatedAt={}",
+            event.wishId,
+            event.ownerId,
+            event.title ?: "-",
+            event.price ?: "-",
+            event.updatedAt
+        )
+    }
+}
