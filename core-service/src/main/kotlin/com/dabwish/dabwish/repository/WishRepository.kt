5da2ec97ba665d39ffc59dd6f84bf7f25f63aef1
Arseniy Alhimovich <144542914+senya-2011@users.noreply.github.com>
2025-11-29 01:39:40 +0300

    fix(WishRepository): add findAllByUserId

--- core-service/src/main/kotlin/com/dabwish/dabwish/repository/WishRepository.kt
+++ core-service/src/main/kotlin/com/dabwish/dabwish/repository/WishRepository.kt
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface WishRepository: JpaRepository<Wish, Long> {
     fun findByUser(user: User): List<Wish>
+    fun findAllByUserId(userId: Long): List<Wish>
 }
