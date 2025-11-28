646d5b6d97cb11feae3abe7e8657dbf91273a543
Arseniy Alhimovich <144542914+senya-2011@users.noreply.github.com>
2025-11-29 01:42:11 +0300

    fix(UserService): java Transactional -> spring Transactional

--- core-service/src/main/kotlin/com/dabwish/dabwish/service/UserService.kt
+++ core-service/src/main/kotlin/com/dabwish/dabwish/service/UserService.kt
@@ -7,7 +7,7 @@ import com.dabwish.dabwish.generated.dto.UserUpdateRequest
 import com.dabwish.dabwish.mapper.UserMapper
 import com.dabwish.dabwish.model.user.User
 import com.dabwish.dabwish.repository.UserRepository
-import jakarta.transaction.Transactional
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
