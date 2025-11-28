15567e4d3e1e5ccc3b69c578c3dc55dd3eb386ee
Arseniy Alhimovich <144542914+senya-2011@users.noreply.github.com>
2025-11-29 01:41:07 +0300

    feat(WishService): add wishService

--- core-service/src/main/kotlin/com/dabwish/dabwish/service/WishService.kt
+++ core-service/src/main/kotlin/com/dabwish/dabwish/service/WishService.kt
@@ -1,8 +1,53 @@
 package com.dabwish.dabwish.service
 
+import com.dabwish.dabwish.exception.UserNotFoundException
+import com.dabwish.dabwish.exception.WishNotFoundException
+import com.dabwish.dabwish.generated.dto.WishRequest
+import com.dabwish.dabwish.generated.dto.WishUpdateRequest
+import com.dabwish.dabwish.mapper.WishMapper
+import com.dabwish.dabwish.model.wish.Wish
+import com.dabwish.dabwish.repository.UserRepository
+import com.dabwish.dabwish.repository.WishRepository
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.stereotype.Service
 
 
 @Service
-class WishService {
+class WishService(
+    private val wishRepository: WishRepository,
+    private val wishMapper: WishMapper,
+    private val userRepository: UserRepository,
+) {
+    fun findAllByUserId(userId: Long): List<Wish> {
+        if (!userRepository.existsById(userId)) {
+            throw UserNotFoundException(userId)
+        }
+        return wishRepository.findAllByUserId(userId)
+    }
+
+    fun findById(id: Long): Wish {
+        return wishRepository.findById(id).
+            orElseThrow{WishNotFoundException(id)}
+    }
+
+    @Transactional
+    fun create(userId: Long, request: WishRequest): Wish {
+        val user = userRepository.findById(userId)
+            .orElseThrow { UserNotFoundException(userId) }
+        val wish = wishMapper.toEntity(request, user)
+        return wishRepository.save(wish)
+    }
+
+    @Transactional
+    fun delete(id: Long){
+        if (!wishRepository.existsById(id)) throw WishNotFoundException(id)
+        wishRepository.deleteById(id)
+    }
+
+    @Transactional
+    fun update(id: Long, wishUpdateRequest: WishUpdateRequest): Wish {
+        val wish = findById(id)
+        wishMapper.updateEntityFromRequest(wishUpdateRequest, wish)
+        return wishRepository.save(wish)
+    }
 }
